39e7026e3636808c3f87fbe6354f6016388ef93b
ChemicalLuck <J.Tebbett@outlook.com>
2022-07-26 14:26:36 +0100

    switched to pyproject

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
+del dist\* /q
 py -m build
+del build\* /q
+pip install --upgrade pip
+pip install -force-reinstall -vvv .
 twine upload dist/*
--- src/key_spyder/defaults.py
+++ src/key_spyder/defaults.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from os import path
+from os import path, makedirs
 
 DEFAULT_PATH = path.expanduser("~/Documents/key-spyder")
+if not path.exists(DEFAULT_PATH):
+    makedirs(DEFAULT_PATH)
 NOW = datetime.now().strftime('%Y-%m-%dT%H%M%SZ')
